9fee522e09e25a92df5c4c2970a5b424c1649ee2
SerJo2 <termenator1ser@gmail.com>
2025-05-24 20:55:27 +1000

    Design

--- FindEndPortal/MainForm.Designer.cs
+++ FindEndPortal/MainForm.Designer.cs
@@ -43,54 +43,56 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.HelpButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // XFirst
             // 
-            this.XFirst.Location = new System.Drawing.Point(78, 68);
+            this.XFirst.Location = new System.Drawing.Point(58, 16);
             this.XFirst.Name = "XFirst";
             this.XFirst.Size = new System.Drawing.Size(100, 20);
             this.XFirst.TabIndex = 0;
             // 
             // ZFirst
             // 
-            this.ZFirst.Location = new System.Drawing.Point(78, 94);
+            this.ZFirst.Location = new System.Drawing.Point(58, 42);
             this.ZFirst.Name = "ZFirst";
             this.ZFirst.Size = new System.Drawing.Size(100, 20);
             this.ZFirst.TabIndex = 1;
             // 
             // AngleFirst
             // 
-            this.AngleFirst.Location = new System.Drawing.Point(78, 120);
+            this.AngleFirst.Location = new System.Drawing.Point(58, 68);
             this.AngleFirst.Name = "AngleFirst";
             this.AngleFirst.Size = new System.Drawing.Size(100, 20);
             this.AngleFirst.TabIndex = 2;
             // 
             // XSecond
             // 
-            this.XSecond.Location = new System.Drawing.Point(336, 68);
+            this.XSecond.Location = new System.Drawing.Point(219, 16);
             this.XSecond.Name = "XSecond";
             this.XSecond.Size = new System.Drawing.Size(100, 20);
             this.XSecond.TabIndex = 3;
             // 
             // ZSecond
             // 
-            this.ZSecond.Location = new System.Drawing.Point(336, 94);
+            this.ZSecond.Location = new System.Drawing.Point(219, 42);
             this.ZSecond.Name = "ZSecond";
             this.ZSecond.Size = new System.Drawing.Size(100, 20);
             this.ZSecond.TabIndex = 4;
             // 
             // AngleSecond
             // 
-            this.AngleSecond.Location = new System.Drawing.Point(336, 120);
+            this.AngleSecond.Location = new System.Drawing.Point(219, 68);
             this.AngleSecond.Name = "AngleSecond";
             this.AngleSecond.Size = new System.Drawing.Size(100, 20);
             this.AngleSecond.TabIndex = 5;
             // 
             // Calculate
             // 
-            this.Calculate.Location = new System.Drawing.Point(689, 389);
+            this.Calculate.Location = new System.Drawing.Point(269, 129);
             this.Calculate.Name = "Calculate";
             this.Calculate.Size = new System.Drawing.Size(99, 49);
             this.Calculate.TabIndex = 6;
@@ -102,7 +104,7 @@
             // 
             this.XEndPortal.AutoSize = true;
             this.XEndPortal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.XEndPortal.Location = new System.Drawing.Point(464, 280);
+            this.XEndPortal.Location = new System.Drawing.Point(136, 116);
             this.XEndPortal.Name = "XEndPortal";
             this.XEndPortal.Size = new System.Drawing.Size(127, 31);
             this.XEndPortal.TabIndex = 7;
@@ -112,7 +114,7 @@
             // 
             this.ZEndPortal.AutoSize = true;
             this.ZEndPortal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ZEndPortal.Location = new System.Drawing.Point(465, 311);
+            this.ZEndPortal.Location = new System.Drawing.Point(137, 147);
             this.ZEndPortal.Name = "ZEndPortal";
             this.ZEndPortal.Size = new System.Drawing.Size(126, 31);
             this.ZEndPortal.TabIndex = 8;
@@ -121,7 +123,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 71);
+            this.label1.Location = new System.Drawing.Point(32, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(20, 13);
             this.label1.TabIndex = 9;
@@ -130,7 +132,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 97);
+            this.label2.Location = new System.Drawing.Point(32, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(20, 13);
             this.label2.TabIndex = 10;
@@ -139,7 +141,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 123);
+            this.label3.Location = new System.Drawing.Point(12, 71);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 11;
@@ -148,7 +150,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(290, 123);
+            this.label4.Location = new System.Drawing.Point(173, 71);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 13);
             this.label4.TabIndex = 14;
@@ -157,7 +159,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(310, 97);
+            this.label5.Location = new System.Drawing.Point(193, 45);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(20, 13);
             this.label5.TabIndex = 13;
@@ -166,28 +168,50 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(310, 71);
+            this.label6.Location = new System.Drawing.Point(193, 19);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(20, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "X2";
             // 
-            // button1
+            // HelpButton
             // 
-            this.button1.Location = new System.Drawing.Point(753, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(35, 35);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "?";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.HelpButton.Location = new System.Drawing.Point(333, 16);
+            this.HelpButton.Name = "HelpButton";
+            this.HelpButton.Size = new System.Drawing.Size(35, 35);
+            this.HelpButton.TabIndex = 15;
+            this.HelpButton.Text = "?";
+            this.HelpButton.UseVisualStyleBackColor = true;
+            this.HelpButton.Click += new System.EventHandler(this.HelpButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(13, 147);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 31);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Portal Z:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(12, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 31);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Portal X:";
             // 
             // CalculateEndPortal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(381, 193);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.HelpButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
@@ -227,7 +251,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button HelpButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- FindEndPortal/MainForm.cs
+++ FindEndPortal/MainForm.cs
@@ -68,7 +68,7 @@ namespace FindEndPortal
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void HelpButton_Click(object sender, EventArgs e)
         {
 
         }
