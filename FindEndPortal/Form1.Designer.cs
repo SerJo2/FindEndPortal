9cc4a67f7c767454fbfad33ef8ced88e7e32e161
SerJo2 <termenator1ser@gmail.com>
2025-05-24 13:30:52 +1000

    Add project files.

---
+++ FindEndPortal/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace FindEndPortal
+{
+    partial class CalculateEndPortal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XFirst = new System.Windows.Forms.TextBox();
+            this.ZFirst = new System.Windows.Forms.TextBox();
+            this.AngleFirst = new System.Windows.Forms.TextBox();
+            this.XSecond = new System.Windows.Forms.TextBox();
+            this.ZSecond = new System.Windows.Forms.TextBox();
+            this.AngleSecond = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.XEndPortal = new System.Windows.Forms.Label();
+            this.ZEndPortal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // XFirst
+            // 
+            this.XFirst.Location = new System.Drawing.Point(78, 68);
+            this.XFirst.Name = "XFirst";
+            this.XFirst.Size = new System.Drawing.Size(100, 20);
+            this.XFirst.TabIndex = 0;
+            // 
+            // ZFirst
+            // 
+            this.ZFirst.Location = new System.Drawing.Point(78, 94);
+            this.ZFirst.Name = "ZFirst";
+            this.ZFirst.Size = new System.Drawing.Size(100, 20);
+            this.ZFirst.TabIndex = 1;
+            // 
+            // AngleFirst
+            // 
+            this.AngleFirst.Location = new System.Drawing.Point(78, 120);
+            this.AngleFirst.Name = "AngleFirst";
+            this.AngleFirst.Size = new System.Drawing.Size(100, 20);
+            this.AngleFirst.TabIndex = 2;
+            // 
+            // XSecond
+            // 
+            this.XSecond.Location = new System.Drawing.Point(336, 68);
+            this.XSecond.Name = "XSecond";
+            this.XSecond.Size = new System.Drawing.Size(100, 20);
+            this.XSecond.TabIndex = 3;
+            // 
+            // ZSecond
+            // 
+            this.ZSecond.Location = new System.Drawing.Point(336, 94);
+            this.ZSecond.Name = "ZSecond";
+            this.ZSecond.Size = new System.Drawing.Size(100, 20);
+            this.ZSecond.TabIndex = 4;
+            // 
+            // AngleSecond
+            // 
+            this.AngleSecond.Location = new System.Drawing.Point(336, 120);
+            this.AngleSecond.Name = "AngleSecond";
+            this.AngleSecond.Size = new System.Drawing.Size(100, 20);
+            this.AngleSecond.TabIndex = 5;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(689, 389);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(99, 49);
+            this.Calculate.TabIndex = 6;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // XEndPortal
+            // 
+            this.XEndPortal.AutoSize = true;
+            this.XEndPortal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.XEndPortal.Location = new System.Drawing.Point(464, 280);
+            this.XEndPortal.Name = "XEndPortal";
+            this.XEndPortal.Size = new System.Drawing.Size(127, 31);
+            this.XEndPortal.TabIndex = 7;
+            this.XEndPortal.Text = "X answer";
+            // 
+            // ZEndPortal
+            // 
+            this.ZEndPortal.AutoSize = true;
+            this.ZEndPortal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ZEndPortal.Location = new System.Drawing.Point(465, 311);
+            this.ZEndPortal.Name = "ZEndPortal";
+            this.ZEndPortal.Size = new System.Drawing.Size(126, 31);
+            this.ZEndPortal.TabIndex = 8;
+            this.ZEndPortal.Text = "Z answer";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Z1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Angle1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(290, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Angle2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(310, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Z2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(310, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "X2";
+            // 
+            // CalculateEndPortal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ZEndPortal);
+            this.Controls.Add(this.XEndPortal);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.AngleSecond);
+            this.Controls.Add(this.ZSecond);
+            this.Controls.Add(this.XSecond);
+            this.Controls.Add(this.AngleFirst);
+            this.Controls.Add(this.ZFirst);
+            this.Controls.Add(this.XFirst);
+            this.Name = "CalculateEndPortal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox XFirst;
+        private System.Windows.Forms.TextBox ZFirst;
+        private System.Windows.Forms.TextBox AngleFirst;
+        private System.Windows.Forms.TextBox XSecond;
+        private System.Windows.Forms.TextBox ZSecond;
+        private System.Windows.Forms.TextBox AngleSecond;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label XEndPortal;
+        private System.Windows.Forms.Label ZEndPortal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FindEndPortal/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindEndPortal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FindEndPortal")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3313d51-660b-48d5-a2b0-fde58d7d4b10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
